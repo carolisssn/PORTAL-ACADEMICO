014d58d5861b0f03b4c667eaf76cf9e4f0ce2761
Ana Carolina Silva de Sousa <rm78486@estudante.fieb.edu.br>
2021-10-18 15:24:51 -0300

    Lógica de autenticação concluída

--- README.md
+++ README.md
@@ -5,3 +5,33 @@
 
 npm i passport passport-local express-session bcrypt
 
+Passport
+Passport é autenticação middleware para Node. Ele foi projetado para servir a um propósito singular: autenticar pedidos
+O passport reconhece que cada aplicativo tem requisitos exclusivos de autenticação. Os mecanismos de autenticação são conhecidos como "estratégias".
+
+# passport-local
+Uma das estratégias do passport, utilizada para autenticação com nome de usuário e senha.
+
+### Dependencias 
+
+npm i passport passport-local express-session bcryptjs
+
+passport 
+(http://www.passportjs.org/docs/downloads/html/)
+
+Passport é autenticação middleware para Node. Ele foi projetado para servir a um propósito singular:
+autenticar pedidos. Ao escrever módulos, o encapsulamento é uma vitrude, então o passport delega todas as outras funcionalidades para o aplicativo.
+
+O passport reconhece que cada aplicativo tem requisitos exclusivos de autenticação. Os mecanismos de 
+autenticação são conhecidos como "estratégias"
+
+passport-local
+(http://www.passportjs.org/packages/passport-local)
+
+Estratégia de passport para autenticação com nome de usuário e senha.
+
+express-session
+
+Quando o cliente faz uma solicitação de login no servidor, o servidor criará uma sessão e armazenará 
+no lado do servidor. Quando o servidor responde ao cliente, ele envia um cookie.
+
--- src/auth.js
+++ src/auth.js
@@ -4,26 +4,62 @@ const LocalStrategy = require('passport-local').Strategy;
 //http://bcrypt-generator.com/
 
 const users =[{
-    _id:1,
-    username:'adm',
-    password:'123', //123
+    _id: 1,
+    username: 'adm',
+    password:'$2a$12$2XB0I1QDMSRLuR0r4e3sWeFIJhrGfvZr6WyR6aXkRb2/g6qDomN02', // 123
     email:'adm@gmail.com'
 
 }];
 
 function findUser(username){
-    return users.find(item => item.username);
+    return users.find(item => item.username === username);
 }
 
-function findUserbyid(ID) {
+function findUserById(ID) {
     return users.find(item =>  item._id === id);
 }
 
-//serializeUser => uma vez autenticado ele salva um cookie no front e uma sessão no back.
-
 module.exports = (passport) => {
-passport.serializeUser((user, done)=>{
-    done(null, user._id);
-});
+ 
+    // Uma vez autenticado ele salva um cookie no front e uma sessão no back
+    passport.serializeUser((user, done)=> {
+     done(null, user._id);
+    });
+
+   // Uma vez que as informações estão gravadas, posso recuperá-las
+    passport.deserializeUser((id, done)=>{
+   try {
+
+    const user = findUserById(ID);
+    done(null, user);
+
+   }catch (error) {
+     console.log(error);
+     return done(error, null);
+ }
+
+    });
+
+passport.use(new LocalStrategy({
+    username: 'username',
+    password: 'password'
+}, (username, password, done)=>{
+    try{
+        const user = findUser(username);
+        if(!user) return done(null, false);
+
+        const isValid = bcrypt.compareSync(password, user.password);
+        if(!isValid) return done(null, false);
+        return (done(null, user));
+    } catch (error){
+        console.log(error);
+        return done(error, false);
+    }
+
+
+ }))
+
+
+}
 
 
